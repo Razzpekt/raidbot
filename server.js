ff501f079da9af371712e71cb5cfa0e7b5e7c279
Tim Weiskopf <tim.weiskopf@gmx.de>
2018-03-30 14:44:35 +0200

    workaround for env variable privatekey

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ require('dotenv').config();
 require('firebase/app');
 require('firebase/database');
 var admin = require("firebase-admin");
+var privateKey = '{"stupidworkaround": "'+ process.env.private_key +'"}';
+privateKey = '' + JSON.parse(privateKey).stupidworkaround + ''
 var firebase = admin.initializeApp({
   credential: admin.credential.cert({
     "type": process.env.type,
     "project_id": process.env.project_id,
     "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
+    "private_key": privateKey,
     "client_email": process.env.client_email,
     "client_id": process.env.client_id,
     "auth_uri": process.env.auth_uri,
